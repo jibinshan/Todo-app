4c04f3b46030e9d5448ccf26a513cce0912968f1
jibinshan <138592544+jibinshan@users.noreply.github.com>
2023-12-15 11:16:19 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDb = async ()=>{
     try {
-        const {connection} = await mongoose.connect(process.env.MONGO_URI)
+        const {connection} = await mongoose.connect("mongodb+srv://shanjibin10:2ZowYqKgxl46mVhO@cluster0.euayt4z.mongodb.net/taktodo?retryWrites=true&w=majority")
         console.log("database connected"+connection.host);
     } catch (error) {
        console.log(error,"=========iiiiiiii"); 
